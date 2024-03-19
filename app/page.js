8acbf0e6e0e97352c63b1f6bce5f67bf01edc795
Sajjad Hussain <sajjadast786@gmail.com>
2024-03-20 01:20:41 +0500

    14

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Form } from "./components/form";
 
 export default function Home() {
   return (
     <main className={styles.main}>
 
       <h2>Let's elevate file storage to the next level by leveraging Vercel Blob, a cost-effective and efficient solution.</h2>
+    
+    <Form />
     </main>
   );
 }
