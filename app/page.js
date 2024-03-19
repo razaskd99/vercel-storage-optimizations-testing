a58c7d239ae285811e3a10e5f6bf20633b7548e7
Sajjad Hussain <sajjadast786@gmail.com>
2024-03-20 01:22:12 +0500

    14

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Form } from "./components/form";
+import { Form } from "./components/Form";
 
 export default function Home() {
   return (
